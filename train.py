a439035f475cf646ec78e17d35a0c70ec0bee004
Santiago <shpotes3@gmail.com>
2021-05-07 17:23:14 -0500

    [WIP] training loop

--- train.py
+++ train.py
@@ -2,11 +2,13 @@
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torch.utils.data as data
 from torchvision import models, datasets, transforms
 from tqdm import tqdm
 
-from safe_regions import hook, region
+from safe_regions.region import MinMaxRegion
+from safe_regions.layers import track_safe_region
 
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -35,22 +37,46 @@ def get_model():
     resnet = models.resnet18()
     return resnet.to(DEVICE)
 
-
-def train():
+def train(
+        lr=3e-4,
+        num_epochs=10,
+        track_safe_regions=False
+):
     loader = get_loader()
     model = get_model()
-    
-    hook.track_safe_region(model, region.MinMaxRegion)
 
-    for input_tensor, _ in tqdm(loader):
-        model(input_tensor.to(DEVICE))
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=lr,
+        momentum=0.9,
+        weight_decay=5e-4,
+    )
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
     
+    if track_safe_regions:
+        track_safe_region(model, MinMaxRegion)
 
-    for layer in model.children():
-        if isinstance(layer, nn.ReLU):
-            reg = layer.__region
-            
-    print(reg._min)
+    for _ in range(num_epochs):
+        train_loss = 0
+        progress = tqdm(loader)
+        for idx, (input_tensor, target) in enumerate(progress):
+            input_tensor, target = input_tensor.to(DEVICE), target.to(DEVICE)
+            output = model(input_tensor)
+            loss = criterion(output, target)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+
+            progress.set_description(f'Loss {train_loss / (idx + 1):.3f}')
+            progress.refresh()
+
+        scheduler.step()
+
+    return model
 
     
 if __name__ == '__main__':
