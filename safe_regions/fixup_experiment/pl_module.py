bead069f071caded7fac70bb4f9d3813046c54ef
Santiago <shpotes3@gmail.com>
2021-06-16 11:56:51 -0500

    improve training

--- safe_regions/fixup_experiment/pl_module.py
+++ safe_regions/fixup_experiment/pl_module.py
@@ -7,20 +7,22 @@ import torch.optim as optim
 import pytorch_lightning as pl
 import torchmetrics.functional as M
 
-from safe_regions.fixup_resnet_cifar import fixup_resnet20
+from safe_regions.fixup_experiment.fixup_resnet_cifar import fixup_resnet20
 from safe_regions.region import Region
 from safe_regions.layers import ReLU
 
-class FixupResNetModule(pl.LightningModule):
+class FixupResNet(pl.LightningModule):
     def __init__(
             self,
             region_cls: Optional[Region] = None,
             num_classes: int = 10,
             learning_rate: float = 3e-4,
+            decay: float = 1e-4,
     ):
         super().__init__()
 
         self.learning_rate = learning_rate
+        self.decay = decay
         self.region_cls = region_cls
 
         self.model = fixup_resnet20(num_classes=num_classes)
@@ -31,8 +33,7 @@ class FixupResNetModule(pl.LightningModule):
             self._track_regions(self.model, ('model',))
 
     def forward(self, input_tensor):
-        hidden_state = self.model(input_tensor)
-        return self.proj(hidden_state)
+        return self.model(input_tensor)
 
     def _common_step(self, batch):
         input_tensor, target = batch
@@ -67,20 +68,29 @@ class FixupResNetModule(pl.LightningModule):
         self.log('val_acc', acc)
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(
-            self.parameters(),
+        parameters_bias = [p[1] for p in self.model.named_parameters() if 'bias' in p[0]]
+        parameters_scale = [p[1] for p in self.model.named_parameters() if 'scale' in p[0]]
+        parameters_others = [p[1] for p in self.model.named_parameters() if not ('bias' in p[0] or 'scale' in p[0])]
+
+        optimizer = optim.SGD(
+            [{'params': parameters_bias, 'lr': self.base_lr/10.},
+             {'params': parameters_scale, 'lr': self.base_lr/10.},
+             {'params': parameters_others}],
             lr=self.learning_rate,
+            momentum=0.9,
+            weight_decay=self.decay,
         )
 
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+        # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
-        return {
-            'optimizer': optimizer,
-            'lr_schedler': {
-                'scheduler': scheduler,
-                'monitor': 'val_loss'
-            }
-        }
+        return optimizer
+    #{
+    #        'optimizer': optimizer,
+    #        'lr_schedler': {
+    #            'scheduler': scheduler,
+    #            'monitor': 'val_loss'
+    #        }
+    #    }
 
     def _track_regions(self, module, parent_name=('',)):
         for idx, (layer_name, layer) in enumerate(module.named_children()):
